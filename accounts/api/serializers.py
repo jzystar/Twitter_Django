c81f7ad6bf3678503747a08d70895b26446aa546
yzjiangyang <yzjiangyang123@163.com>
2021-12-13 21:59:24 -0800

    user-authentication-api

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,54 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from rest_framework import exceptions
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = ['username', 'email']
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+    def validate(self, data):
+        if not User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'username': 'User does not exist.'
+            })
+        return data
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=20, min_length=6)
+    password = serializers.CharField(max_length=20, min_length=6)
+    email = serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+    # will be called when is_valid is called
+    def validate(self, data):
+        # 增加验证 username 是不是只由给定的字符集合构成.
+        if not User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'username': 'This username has been used.'
+            })
+        if User.objects.filter(email=data['email'].lower()).exists():
+            raise exceptions.ValidationError({
+                'email': 'This email has been used.'
+            })
+
+        return data
+
+    def create(self, validated_data):
+        username = validated_data['username'].lower()
+        email = validated_data['email'].lower()
+        password = validated_data['password']
+        user = User.objects.create_user(
+            username = username,
+            email = email,
+            password = password
+        )
+
+        return user
