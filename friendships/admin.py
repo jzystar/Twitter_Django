30f5d234a608bae0c476018db08db4ab43a0cf1c
yzjiangyang <yzjiangyang123@163.com>
2022-01-06 16:37:01 -0800

    add newsfeeds model

--- friendships/admin.py
+++ friendships/admin.py
@@ -3,7 +3,7 @@ from friendships.models import Friendship
 
 
 @admin.register(Friendship)
-class Friendship(admin.ModelAdmin):
+class FriendshipAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_at'
     list_display = (
         'from_user',
