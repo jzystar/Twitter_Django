826f59e71630657451a473d9040f9690112f263a
yzjiangyang <yzjiangyang123@163.com>
2022-01-24 23:17:47 -0800

    Revert "Revert "add comments to tweets""
    This reverts commit 202d910fba04015f0141a88f68f9fab7da740b36.

--- tweets/api/serializers.py
+++ tweets/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from tweets.models import Tweet
 from accounts.api.serializers import UserSerializerForTweet
+from comments.api.serializers import CommentSerializer
 
 
 class TweetSerializer(serializers.ModelSerializer):
@@ -24,3 +25,11 @@ class TweetSerializerForCreate(serializers.ModelSerializer):
         tweet = Tweet.objects.create(user=user, content=content)
 
         return tweet
+
+
+class TweetSerializerWithComment(serializers.ModelSerializer):
+    comments = CommentSerializer(source="comment_set", many=True)
+
+    class Meta:
+        model = Tweet
+        fields = ('id', 'user', 'created_at', 'content', 'comments')
