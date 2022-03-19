3ba8bd6413def0d398533227ce693a05ca45a72c
yzjiangyang <yzjiangyang123@163.com>
2022-03-19 13:30:43 -0700

    Revert "cache likes and comments count in redis"
    This reverts commit 787947bd00951dfcf5333bed4a401a01bc1e9a91.

--- comments/listeners.py
+++ comments/listeners.py
@@ -1,24 +1,14 @@
-from utils.redis_helper import RedisHelper
-
 def incr_comments_count(sender, instance, created, **kwargs):
     from django.db.models import F
     from tweets.models import Tweet
 
     if not created:
         return
-
     # .update will not trigger post_save listener
-    # update count in Tweet table
     Tweet.objects.filter(id=instance.tweet_id).update(comments_count=F('comments_count') + 1)
-    # update count in redis
-    RedisHelper.incr_count(instance.tweet, 'comments_count')
 
 def decr_comments_count(sender, instance, **kwargs):
     from django.db.models import F
     from tweets.models import Tweet
 
-    # .update will not trigger post_save listener
-    # update count in Tweet table
     Tweet.objects.filter(id=instance.tweet_id).update(comments_count=F('comments_count') - 1)
-    # update count in redis
-    RedisHelper.decr_count(instance.tweet, 'comments_count')
